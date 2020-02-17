e1628fb0b84dff54f4976269dc982f3d497513d5
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2020-02-17 21:07:17 +0530

    devcon push notification integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {SunbirdSdk} from '@project-sunbird/sunbird-sdk';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { NotificationService } from './services/notification.service';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { OneSignal } from '@ionic-native/onesignal/ngx';
 
 declare global {
   interface Window {
@@ -349,6 +350,7 @@ export const sunbirdSdkFactory =
     SplashScreen,
     LocalNotifications,
     NotificationService,
+    OneSignal,
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
     ...sunbirdSdkServicesProvidersFactory(),
     {provide: APP_INITIALIZER, useFactory: sunbirdSdkFactory, deps: [], multi: true}
